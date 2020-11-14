a822a63281f580718c7b525cf956225cabfcf245
binding-proc-8938 <naomirhames2020@gmail.com>
2020-11-14 17:55:47 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,93 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+def turn(count)
+  counter = 0 
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+end
+
+def current_player(board)
+end
+WIN_COMBINATIONS =[
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2],
+]
+def won?(board)
+        WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+     
+      position_1 = board[win_index_1] 
+      position_2 = board[win_index_2] 
+      position_3 = board[win_index_3] 
+    
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination # return the win_combination indexes that won.
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      else
+        false
+      end
+    end
+    false
+  end
+
+ def full?(board) 
+  board.all?{ |space| 
+    space == "X" || space ==  "O" 
+  }
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+ end
+ 
+ def over?(board)
+   full?(board) || won?(board)
+ end
+def winner(board)
+  
+end
+
